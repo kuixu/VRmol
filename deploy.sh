17d3e9b9e14373c6a2f66757810534f4edf4ef6b
XU Kui <xukui.cs@gmail.com>
2018-08-04 19:05:24 +0800

    add deploy code

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mv index.html index_dev.html
-mv index_out.html index.html
+cp index_out.html index.html
+cd libs/molecule/
 uglifyjs extension.js  math.js w3m.js  static.js PDBFileCore.js  PDBFileLoader.js PDBFileRender.js PDBFileTool.js PDBFileDrawer.js PDBFilePainter.js PDBFileController.js  index.js \
 -c -m -o inano.min.js 
 #-c -m --mangle-props -o inano.min.js 
