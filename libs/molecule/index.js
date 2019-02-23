84caca486a52074ed68cd19acd4a839ae139bfed
Kui Xu <xukui.cs@gmail.com>
2019-02-23 15:45:28 +0800

    reformat index.html

--- libs/molecule/index.js
+++ libs/molecule/index.js
@@ -1,4 +1,51 @@
 // =====================
+// Menu
+// =====================
+
+if (document.getElementById){ 
+    document.write('<style type="text/css">\n');
+    document.write('.submenu{display: none;}\n');
+    document.write('</style>\n');
+    switchMenu("mainStructure");
+}
+function switchRightMenu(obj){
+    if(document.getElementById){
+        var el = document.getElementById(obj);
+        var ar = document.getElementById("rightmenu").getElementsByTagName("span"); //DynamicDrive.com change
+        if(el.style.display != "block"){ 
+            for (var i=0; i<ar.length; i++){
+                if (ar[i].className=="rightsubmenu") 
+                    ar[i].style.display = "none";
+            }
+            el.style.display = "block";
+        }else{
+            el.style.display = "none";
+        }
+    }
+}
+
+function switchMenu(obj){
+    if(document.getElementById){
+        var el = document.getElementById(obj);
+        var ar = document.getElementById("topmenu").getElementsByTagName("span"); //DynamicDrive.com change
+        if(el.style.display != "block"){ 
+            for (var i=0; i<ar.length; i++){
+                if (ar[i].className=="submenu") 
+                    ar[i].style.display = "none";
+            }
+            el.style.display = "block";
+        }else{
+            el.style.display = "none";
+        }
+    }
+}
+// var worker = new Worker('libs/fileutils/export.js');
+// (function(){
+
+// })()
+
+
+// =====================
 // parse api parameter
 // =====================
 
