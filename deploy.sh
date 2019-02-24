44bf855eebc286b11262a2c89a5903e392be2562
Kui <barrykui@users.noreply.github.com>
2019-02-25 00:57:13 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp index_out.html index.html
-cd libs/molecule/
-uglifyjs extension.js  math.js w3m.js  static.js PDBFileCore.js  PDBFileLoader.js PDBFileRender.js PDBFileTool.js PDBFileDrawer.js PDBFilePainter.js PDBFileController.js  index.js \
--c -m -o inano.min.js 
-#-c -m --mangle-props -o inano.min.js 
-#-c -o inano.min.js 
